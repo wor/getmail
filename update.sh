36b5dbd77e84df7e25bcc7fc32c408328fa68759
Esa Määttä <wor@iki.fi>
2012-10-31 21:21:10 +0200

    Added script to update usptream branch

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Quick script to automate repo updating from upstream tarball releases.
+
+set -o errexit
+
+[[ "${1}" ]] || { echo "Error: First parameter should be version number like 4.35.0"; exit 1; }
+
+repo="/home/wor/repo/getmail"
+branch="upstream"
+version="${1}"
+
+url="http://pyropus.ca/software/getmail/old-versions/"
+package="getmail-${version}.tar.gz"
+
+cd /tmp
+git clone -b "${branch}" "${repo}" "getmail-${version}"
+cd "getmail-${version}"
+
+wget "${url}${package}"
+tar -xavf "${package}" --overwrite --strip-components 1
+rm "${package}"
+git add .
+
+awk "BEGIN { out_fn=\"none\" }/^Version / { out_fn=\"/dev/null\"; } /^Version ${version}/ { out_fn=\"/dev/stdout\"; } { print > out_fn; }" docs/CHANGELOG \
+    | git commit -F -
+
+git push origin "${branch}":"${branch}"
